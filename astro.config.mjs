c07ceb9c05e9ad0a2cf98116ecd73ca32db1a08d
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-10-13 15:55:08 -0400

    deploy as GH Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://chadoh.com',
+  base: '/entire-life-offline-first',
+  integrations: [react()],
 });
