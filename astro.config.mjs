e10c8bf553f557d5ecad3f19326d9ebb266c4b3d
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-05-05 11:25:08 -0400

    try a simpler Vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,25 +12,10 @@ export default defineConfig({
     build: {
       lib: {
         // Could also be a dictionary or array of multiple entry points
-        entry: {
-          googleWorker: new URL('src/data/backends/google/worker.ts', import.meta.url),
-        },
+        entry: new URL('src/data/backends/google/worker.ts', import.meta.url),
         formats: ['es'],
         // set fileName to, for example, `googleWorker.js`
-        fileName: (format, entryName) => `${entryName}.${format}`,
-      },
-      rollupOptions: {
-        // make sure to externalize deps that shouldn't be bundled
-        // into your library, like React (should be unnecessary for workers,
-        // since they don't import DOM stuff)
-        external: [],
-        output: {
-          // Provide global variables to use in the UMD build
-          // for externalized deps
-          // (again, probably not necessary for workers)
-          globals: {
-          },
-        },
+        fileName: 'googleWorker',
       },
     },
   },
